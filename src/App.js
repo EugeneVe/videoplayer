93f368e0ae3643e2222e0755aabcf95c450eb678
Eugene <eugene.veprytskyi@gmail.com>
2022-04-18 12:30:12 +0100

    add mobile styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Main from './pages/Main/Main';
-import './App.scss';
 
 function App() {
   return (
-    <div className="app-wrapper">
+    <>
       <Main />
-    </div>
+    </>
   );
 }
 
